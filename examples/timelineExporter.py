6d115154c6dbb78e316b6a11ad599c98bc4f6268
Dennis Greguhn <dennis@greguhn.de>
2021-04-01 19:24:33 +0200

    Environment with timeline exporter

---
+++ examples/timelineExporter.py
@@ -0,0 +1,30 @@
+import sys
+sys.path.append('../')
+from trapi.api import TrBlockingApi
+
+from environment import *
+
+import json
+import time
+
+tr = TrBlockingApi(NUMBER, PIN)
+tr.login()
+
+data = []
+after = ""
+
+try:
+    while True:
+        res = tr.hist(after=after)
+        print(res)
+        for d in res["data"]:
+            data.append(d)
+        after = res["cursors"]["after"]
+        time.sleep(1)
+except:
+    print("finished!")
+
+
+# Write JSON file
+with open('./myTimeline.json', 'w') as f:
+    json.dump(data, f, indent="\t")
