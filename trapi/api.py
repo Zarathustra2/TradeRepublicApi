fa95dd0617045b11b91f9d3e0ff51076dc785ee8
Zarathustra2 <dario.heinisch@gmail.com>
2021-05-22 10:40:34 +0200

    Update

--- trapi/api.py
+++ trapi/api.py
@@ -218,6 +218,18 @@ class TRApi:
             key=f"timelineDetail {id}",
         )
 
+    async def stock_history(self, isin, range="max", callback=print):
+        l = ["1d", "5d", "1m", "3m", "1y", "max"]
+        if range not in l:
+            raise Exception(f"Range of time must be either one of {l}") 
+
+        return await self.sub(
+            "aggregateHistory",
+            payload={"type": "aggregateHistory", "range": range, "id": f"{isin}.LSX"},
+            callback=callback,
+            key=f"aggregateHistory {isin} {range}",
+        )
+
     async def start(self, receive_one=False):
         async with self.mu:
             if self.started:
@@ -378,6 +390,11 @@ class TrBlockingApi(TRApi):
     def stock_details(self, isin):
         return asyncio.get_event_loop().run_until_complete(
             self.get_one(super().stock_details(isin))
+        )
+
+    def stock_history(self, isin, range="max"):
+        return asyncio.get_event_loop().run_until_complete(
+            self.get_one(super().stock_history(isin, range=range))
         )
 
     def hist_event(self, id):
