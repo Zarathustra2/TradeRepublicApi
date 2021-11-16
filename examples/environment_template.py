b4b0b33310400ab843a507eeabbc85a65484e156
nazri480 <nazri48@outlook.com>
2021-11-16 12:31:25 +0100

    Script now asks for PIN if none is given in env.py

--- examples/environment_template.py
+++ examples/environment_template.py
@@ -1,6 +1,11 @@
+import getpass4 as getpass
+
 # Trade Republic Login
 NUMBER = "+491731234567"
-PIN = "1234"
+PIN = ""
 
 # Default to English if not specified when calling the API
 LOCALE = "en"
+
+if not PIN:
+    PIN = getpass.getpass("Pin:")
