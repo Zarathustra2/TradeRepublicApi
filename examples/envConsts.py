92beacf35deb76aab26618674d1ea3bef3dff875
Blessio <24902257+blessio@users.noreply.github.com>
2022-01-03 02:04:22 +0100

    FEAT/Example - Download all trade data for an account timeline in JSON format plus all PDF files (#21)
    * Script for total download added. improved gitignore. Tested only on LINUX.
    
    Co-authored-by: Blessio <blessio.blog@blessio.com>

---
+++ examples/envConsts.py
@@ -0,0 +1,16 @@
+##############################################################################################
+# Modify these Constants to change the file names and paths
+##############################################################################################
+S_OUTPUT_DIR     = "./_storedOutputs/"
+S_DOC_DOWNLOADS  = "./_docDownloads/"
+S_TIMELN_JSNFILE = "myTimeline.json"
+S_TRANSA_CSVFILE = "myTransactions.csv"
+S_TIMELD_JSNFILE = "myTimelineDetails.json"
+
+
+##############################################################################################
+# Do not modify these manually
+S_DOCDOWNLOADS_PATH = S_DOC_DOWNLOADS
+S_TIMELN_JSNFILE_PATH = S_OUTPUT_DIR + S_TIMELN_JSNFILE
+S_TRANSA_CSVFILE_PATH = S_OUTPUT_DIR + S_TRANSA_CSVFILE
+S_TIMELD_JSNFILE_PATH = S_OUTPUT_DIR + S_TIMELD_JSNFILE
---
+++ startMe.sh
@@ -0,0 +1,4 @@
+python3 ./examples/timelineExporterWithDocsAndDetails.py
+# not done yet
+# python3 ./examples/timelineCsvConverter.py
+# cp myTransactions.csv ~/OneDrive/000.10.WorkDay/21C.Dec.2021/
