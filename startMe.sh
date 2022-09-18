5e8bac9a016613f07b0c7ccf3ebc105e8b884e47
Blessio <blessio.blog@blessio.com>
2022-09-18 12:33:57 +0200

    add minimal how-to use the use-case hint documentation and example file

--- startMe.sh
+++ startMe.sh
@@ -1,8 +1,12 @@
-
+echo "* this Python VENV is not required but _strongly_ recommended"
 source venv/bin/activate
 
 echo "* 1st: timeline and AND documents extraction from Trade Republic."
 echo "*      NEEDS to login to trade republic"
+echo "* NOTE: the script will download the complete timeline as JSON"
+echo "*       Documents are stored in a relative path as per envConsts.py"
+echo "*       Thus, if the folder is not cleared each run will download"
+echo "*       only new documents"
 echo "********************************************************************"
 python3 ./examples/timelineExporterWithDocsAndDetails.py
 
@@ -10,8 +14,14 @@ echo " "
 echo "********************************************************************"
 echo "* 2nd: Generate a CSV file from the data extracted above"
 echo "*      DOES _NOT_ NEED to login to trade republic"
+echo "*      - new entries, which you can copy paste to your" 
+echo "*        ignoredEntries.json file will be generated. it is suggested"
+echo "*        that you add paste them there _only_  after you are sure that"
+echo "*        no additional processing is needed"
+echo "*      - new entries, which you can copy paste to your"
+echo "*        companyNameIsins.json file will also be generated"
+echo "* Recommended is that you create a private repo in which you keep these files"
 echo "********************************************************************"
-# not done yet
 python3 ./examples/timelineCSVconvWithDetails.py 
 
 # python3 ./examples/timelineExporterWithDocsAndDetails.py
